1632d8d1cf786845aa8e001ffb0e9c552ae400c8
Maksym Halych <maksym.halych.shi.2022@lpnu.ua>
2024-03-08 09:52:45 +0200

    lab_2 update 1

--- lab_2/main.cpp
+++ lab_2/main.cpp
@@ -17,21 +17,9 @@ void init(Matrix &A, Matrix &B) {
     }
 }
 
-void calculate_static(const Matrix &mat, Vector &res) {
-#pragma omp parallel for schedule(static, 6)
-    for(int i = 0; i < N; i++) {
-        int count = 0;
-        for(int j = 0; j < N; j++) {
-            if(mat[i][j] < 0) {
-                count++;
-            }
-        }
-        res[i] = count;
-    }
-}
-
-void calculate_dynamic(const Matrix &mat, Vector &res) {
-#pragma omp parallel for schedule(dynamic, 4)
+void calculate(const Matrix &mat, Vector &res, omp_sched_t type, int chunk) {
+    omp_set_schedule(type, chunk);
+    #pragma omp parallel for schedule(runtime)
     for(int i = 0; i < N; i++) {
         int count = 0;
         for(int j = 0; j < N; j++) {
@@ -49,13 +37,13 @@ int main() {
     auto C = Vector(N), D = Vector(N);
 
     auto start = omp_get_wtime();
-    calculate_static(A, C);
+    calculate(B, D, omp_sched_static, 6);
     auto end = omp_get_wtime();
     auto time_static = end-start;
     std::cout << "Time for (static, 6) " << time_static << "\n";
 
     start = omp_get_wtime();
-    calculate_dynamic(B, D);
+    calculate(B, D, omp_sched_dynamic, 4);
     end = omp_get_wtime();
     auto time_dynamic = end-start;
     std::cout << "Time for (dynamic, 4) " << time_dynamic << "\n\n";
